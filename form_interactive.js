8a3a5bba13682e03efad00f881367181427b225b
serakoc <cpserakoc@outlook.fr>
2020-02-18 16:22:31 +0100

    a faire : regex et attribution

--- form_interactive.js
+++ form_interactive.js
@@ -1,14 +1,39 @@
 var tableau_de_valeur = [];
 var compteur = 1;
 var value_input = undefined;
+ 
 var tableau_de_titre = [
-    "Votre nom :","Votre prenom :","Votre adresse :","Votre ville :","Votre télephone :"
+    "Votre nom :",
+    "Votre prenom :",
+    "Votre sexe :",
+    "Votre date de naissance :",
+    "Votre adresse :",
+    "Votre code postal :",
+    "Votre ville",
+    "Votre email :",
+    "Votre sujet :",
+    "Votre demande :",
+    "J'accepte le traitement informatique de ce formulaire"
 ];
 var tab_id = 
 [
-    "nom","prenom","adresse","ville","télephone"
+    "nom",
+    "prenom",
+    "sexe",
+    "date",
+    "adresse",
+    "postal",
+    "ville",
+    "email",
+    "sujet",
+    "demande",
+    "confirmation"
+];
 
-]
+var nbr_etape = 0;
+var etape_plus = 100/tableau_de_titre.length;
+
+
 $("#titre").text(tableau_de_titre[0]);
 
 $("#valeur").click(function()
@@ -16,15 +41,16 @@ $("#valeur").click(function()
     //Prend la valeur de l'input et l'attribut a une variable.
     value_input = $("#cobaye").val();
 
-    //Supprime l'erreur car nouvelle insertion 
+    //Supprime l'erreur car c'est nouvelle insertion 
     $("#danger").remove();
-    if(value_input != undefined && value_input != "")
+    if(value_input != undefined && value_input != "" && compteur != 4)
     {
         //Affiche une value différente lorsque le derniers champs du 
         //formulaire est atteint.
         if(compteur+1 == tableau_de_titre.length)
         {
             $("#valeur").val("Valider le formulaire");
+            $("#cobaye").attr("placeholder","taper 'ok' pour valider !");
         }
 
         //insérer la valeur de l'input dans le tableau en derniere position.
@@ -56,15 +82,54 @@ $("#valeur").click(function()
         }
         //on incrémente le compteur seulement quand le champs n'est pas vide 
         //donc considérer comme valider.
-        compteur++; 
+        
+    
+        if(compteur == 2)
+        {
+            $("#cobaye").hide();
+            $("#div_sexe").show();
+        }
+        compteur++;
+        nbr_etape+=etape_plus;
+        $("#progression-bar").width(nbr_etape+"%");
+    }
+    else if(compteur == 3)
+    {
+        // n'est pas finie manque l'attribution a une variable
+        var h = $("#h").is(':checked');
+        var f = $("#f").is(':checked');
+        console.log(h + " " + f);
+        $("#titre").remove();
+        $("#insert").prepend('<p id="titre"></p>')
+        $("#titre").text(tableau_de_titre[compteur]); 
+        $("#div_sexe").hide();
+        $("#date_input").show();
+
+        //ici gérer le cas pour compteur 3 donc la date de naissance
+        compteur++;
+        nbr_etape+=etape_plus;
+        $("#progression-bar").width(nbr_etape+"%");
+    }
+    else if(compteur == 4)
+    {
+        //gerer l'attriution des variables 
+        $("#titre").remove();
+        $("#insert").prepend('<p id="titre"></p>')
+        $("#titre").text(tableau_de_titre[compteur]); 
+        $("#date_input").hide();
+        $("#cobaye").show();
+        console.log($("#date_input").val());
+        compteur++;
+        nbr_etape+=etape_plus;
+        $("#progression-bar").width(nbr_etape+"%");
     }
     //si le champs de l'input est vide alors on 
     //affiche simplement l'erreur concernée.
     else
     {
-        console.log("ok");
         document.getElementById("insert").insertAdjacentHTML("beforeend","<span id='danger'>Votre input est vide !</span>");
     }
+    console.log(compteur);
 });
 
 function envoie_form()
